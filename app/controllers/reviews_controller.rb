811cc94bd05bdf5750c20c2661e6f3d7d89cb707
Mel Chua <mchuam@gmail.com>
2018-05-26 04:08:02 +0000

    added step 11

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def create
+    @product = Product.find params[:product_id]
+    review = Review.new(review_params)
+    review.product_id = params[:product_id]
+    review.user_id = current_user.id
+      if review.save
+        redirect_to :back
+      else
+        redirect_to :back
+      end
+  end
+  private
+    def review_params
+      params.require(:review).permit(:rating, :description)
+    end
 end
